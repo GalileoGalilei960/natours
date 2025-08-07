af79e5895209764c3a1a1934f4d6437d3c048230
GalileoGalilei960 <haladiiostap@gmail.com>
2025-08-07 12:00:13 +0300

    preparing for deploy 2

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,7 +9,7 @@ export const bookTour = async (tourId) => {
     try {
         // 1) Get checkout session from API
         const session = await axios(
-            `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+            `/api/v1/bookings/checkout-session/${tourId}`,
         );
         // console.log(session);
 
@@ -18,7 +18,7 @@ export const bookTour = async (tourId) => {
             sessionId: session.data.session.id,
         });
     } catch (err) {
-        // console.log(err);
+        console.log(err);
         showAlert('error', err);
     }
 };
